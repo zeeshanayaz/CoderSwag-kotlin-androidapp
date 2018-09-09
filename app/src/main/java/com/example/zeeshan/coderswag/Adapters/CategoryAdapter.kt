416392e7e595be3bdb692f9d66a21740320bc587
Zeeshan Ayaz <zeeshanayaz1@gmail.com>
2018-09-09 12:59:21 +0500

    Categories List View

---
+++ app/src/main/java/com/example/zeeshan/coderswag/Adapters/CategoryAdapter.kt
@@ -0,0 +1,46 @@
+package com.example.zeeshan.coderswag.Adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.example.zeeshan.coderswag.Model.Category
+import com.example.zeeshan.coderswag.R
+
+class CategoryAdapter(context: Context, categories: List<Category>) : BaseAdapter() {
+
+    val context = context
+    val categories = categories
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val categoryView : View
+
+        categoryView = LayoutInflater.from(context).inflate(R.layout.category_list_item, null)
+        val categoryImage : ImageView = categoryView.findViewById(R.id.categoryImage)
+        val categoryName : TextView = categoryView.findViewById(R.id.categoryName)
+
+        val category = categories[position]
+
+        val resourceId = context.resources.getIdentifier(category.image, "drawable", context.packageName)
+        categoryImage.setImageResource(resourceId)
+        println(resourceId)
+        categoryName.text = category.title
+        return categoryView
+    }
+
+    override fun getItem(position: Int): Any {
+        return categories[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return 0
+    }
+
+    override fun getCount(): Int {
+        return categories.count()
+    }
+
+}
--- app/src/main/java/com/example/zeeshan/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/example/zeeshan/coderswag/Controller/MainActivity.kt
@@ -3,22 +3,22 @@ package com.example.zeeshan.coderswag.Controller
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import com.example.zeeshan.coderswag.Adapters.CategoryAdapter
 import com.example.zeeshan.coderswag.Model.Category
 import com.example.zeeshan.coderswag.R
+import com.example.zeeshan.coderswag.R.id.categoryListView
 import com.example.zeeshan.coderswag.Services.DataService
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter : ArrayAdapter<Category>
+    lateinit var adapter : CategoryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = ArrayAdapter(this,
-                android.R.layout.simple_list_item_1,
-                DataService.categories)
+        adapter = CategoryAdapter(this, DataService.categories)
         categoryListView.adapter = adapter
     }
 
